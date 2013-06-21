c8b6fcdcace33757a93dba138cea266d519d00ef
Stefan Hendriks <stefanhen83@gmail.com>
2013-06-21 17:06:29 +0200

    use 'let' in spec for prices url

--- spec/model/prices_url_spec.rb
+++ spec/model/prices_url_spec.rb
@@ -10,12 +10,13 @@ describe PricesUrl do
     expect { PricesUrl.new(nil) }.to raise_error(PricesUrl::InvalidURL, "You must give an url, ie http://www.ns.nl/api")
   end
 
+  let(:prices_url) { PricesUrl.new("hostname") }
+
   it "assumes date is now, when not given" do
     @now = DateTime.new(2013, 6, 21)
     Timecop.freeze(@now)
-    pricesUrl = PricesUrl.new("hostname")
     expected_date = "21062013"
-    pricesUrl.to_s.should == "hostname?from=Amsterdam&to=Purmerend&date=#{expected_date}"
+    prices_url.to_s.should == "hostname?from=Amsterdam&to=Purmerend&date=#{expected_date}"
   end
 
 end
